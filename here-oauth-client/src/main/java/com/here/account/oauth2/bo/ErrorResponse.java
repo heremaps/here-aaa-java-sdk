39c754b7ae81e3ed14eb6db0cca6925a55bc2011
Kenneth McCracken <ken.mccracken@here.com>
2016-07-05 23:26:18 -0400

    javadocs on ErrorResponse.

--- here-oauth-client/src/main/java/com/here/account/oauth2/bo/ErrorResponse.java
+++ here-oauth-client/src/main/java/com/here/account/oauth2/bo/ErrorResponse.java
@@ -29,6 +29,61 @@ com.here.account.bo.AuthenticationHttpException: HTTP status code 401, body
  *
  */
 public class ErrorResponse {
+    
+    /**
+     * Not yet implemented.
+     * To be populated when HEREACCT-3541 is implemented and released.
+     * 
+     * The error value.
+     * From OAuth2.0 <a href="https://tools.ietf.org/html/rfc6749#section-5.2">Error Response</a> 
+     * section, the following parameter: 
+     * error
+         REQUIRED.  A single ASCII [USASCII] error code from the
+         following:
+
+         invalid_request
+               The request is missing a required parameter, includes an
+               unsupported parameter value (other than grant type),
+               repeats a parameter, includes multiple credentials,
+               utilizes more than one mechanism for authenticating the
+               client, or is otherwise malformed.
+
+         invalid_client
+               Client authentication failed (e.g., unknown client, no
+               client authentication included, or unsupported
+               authentication method).  The authorization server MAY
+               return an HTTP 401 (Unauthorized) status code to indicate
+               which HTTP authentication schemes are supported.  If the
+               client attempted to authenticate via the "Authorization"
+               request header field, the authorization server MUST
+               respond with an HTTP 401 (Unauthorized) status code and
+               include the "WWW-Authenticate" response header field
+               matching the authentication scheme used by the client.
+
+         invalid_grant
+               The provided authorization grant (e.g., authorization
+               code, resource owner credentials) or refresh token is
+               invalid, expired, revoked, does not match the redirection
+               URI used in the authorization request, or was issued to
+               another client.
+
+         unauthorized_client
+               The authenticated client is not authorized to use this
+               authorization grant type.
+
+         unsupported_grant_type
+               The authorization grant type is not supported by the
+               authorization server.
+
+         invalid_scope
+               The requested scope is invalid, unknown, malformed, or
+               exceeds the scope granted by the resource owner.
+
+         Values for the "error" parameter MUST NOT include characters
+         outside the set %x20-21 / %x23-5B / %x5D-7E.
+     * 
+     */
+    private final String error;
 
     /**
      * A unique error identifier, useful for support questions, 
@@ -57,17 +112,77 @@ public class ErrorResponse {
     private final String message;
     
     public ErrorResponse() {
-        this(null, null, null, null);
+        this(null, null, null, null, null);
     }
     
-    public ErrorResponse(String errorId,
-      Integer httpStatus,
-      Integer errorCode,
-      String message) {
+    public ErrorResponse(String error, 
+          String errorId,
+          Integer httpStatus,
+          Integer errorCode,
+          String message) {
+        this.error = error;
         this.errorId = errorId;
         this.httpStatus = httpStatus;
         this.errorCode = errorCode;
         this.message = message;
+    }
+    
+    /**
+     * Not yet implemented.
+     * To be populated when HEREACCT-3541 is implemented and released.
+     * 
+     * The error value.
+     * From OAuth2.0 <a href="https://tools.ietf.org/html/rfc6749#section-5.2">Error Response</a> 
+     * section, the following parameter: 
+     * error
+         REQUIRED.  A single ASCII [USASCII] error code from the
+         following:
+
+         invalid_request
+               The request is missing a required parameter, includes an
+               unsupported parameter value (other than grant type),
+               repeats a parameter, includes multiple credentials,
+               utilizes more than one mechanism for authenticating the
+               client, or is otherwise malformed.
+
+         invalid_client
+               Client authentication failed (e.g., unknown client, no
+               client authentication included, or unsupported
+               authentication method).  The authorization server MAY
+               return an HTTP 401 (Unauthorized) status code to indicate
+               which HTTP authentication schemes are supported.  If the
+               client attempted to authenticate via the "Authorization"
+               request header field, the authorization server MUST
+               respond with an HTTP 401 (Unauthorized) status code and
+               include the "WWW-Authenticate" response header field
+               matching the authentication scheme used by the client.
+
+         invalid_grant
+               The provided authorization grant (e.g., authorization
+               code, resource owner credentials) or refresh token is
+               invalid, expired, revoked, does not match the redirection
+               URI used in the authorization request, or was issued to
+               another client.
+
+         unauthorized_client
+               The authenticated client is not authorized to use this
+               authorization grant type.
+
+         unsupported_grant_type
+               The authorization grant type is not supported by the
+               authorization server.
+
+         invalid_scope
+               The requested scope is invalid, unknown, malformed, or
+               exceeds the scope granted by the resource owner.
+
+         Values for the "error" parameter MUST NOT include characters
+         outside the set %x20-21 / %x23-5B / %x5D-7E.
+     * 
+     * @return the error
+     */
+    public String getError() {
+        return error;
     }
 
     /**
