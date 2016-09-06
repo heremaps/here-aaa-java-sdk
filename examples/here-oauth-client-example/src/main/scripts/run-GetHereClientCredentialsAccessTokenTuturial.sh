2cfbd05889c1294f70d9e3b7325b7037187a3647
Kenneth McCracken <ken.mccracken@here.com>
2016-09-06 16:04:22 -0400

    remove script.  going with executable jar option

--- examples/here-oauth-client-example/src/main/scripts/run-GetHereClientCredentialsAccessTokenTuturial.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SCRIPTDIR=`dirname $0`
-
-$JAVA_HOME/bin/java -cp ${SCRIPTDIR}/here-oauth-client-example-${my.version}.jar:${SCRIPTDIR}/../here-oauth-client-${my.version}.jar com.here.account.oauth2.tutorial.GetHereClientCredentialsAccessTokenTuturial $@
