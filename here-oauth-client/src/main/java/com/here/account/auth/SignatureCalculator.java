6886c967801fb303c53fb7e7858f476305d98230
Rajagopalan Srividya <srividya.rajagopalan@here.com>
2017-05-30 12:27:57 -0400

    Remove unused import

--- here-oauth-client/src/main/java/com/here/account/auth/SignatureCalculator.java
+++ here-oauth-client/src/main/java/com/here/account/auth/SignatureCalculator.java
@@ -26,6 +26,7 @@ import java.security.KeyFactory;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Signature;
+import java.security.interfaces.ECPublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.ArrayList;
