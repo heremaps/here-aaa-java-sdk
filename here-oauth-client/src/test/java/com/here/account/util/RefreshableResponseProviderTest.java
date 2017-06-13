738c4ad283bb3cc17afb520ac19ad9099b09c01b
Kenneth McCracken <ken.mccracken@here.com>
2017-06-13 14:19:27 -0400

    JavaHttpProviderTest: add unit test coverage for Java HttpProvider implementation.

--- here-oauth-client/src/test/java/com/here/account/util/RefreshableResponseProviderTest.java
+++ here-oauth-client/src/test/java/com/here/account/util/RefreshableResponseProviderTest.java
@@ -1,5 +1,7 @@
 package com.here.account.util;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -63,6 +65,62 @@ public class RefreshableResponseProviderTest {
     public void tearDown() {
         this.refreshableResponseProvider.shutdown();
     }
+    
+    @Test
+    public void test_refreshIntervalMillis() {
+        refreshIntervalMillis = 100L;
+        initialToken = new MyExpiringResponse();
+        refreshTokenFunction = new ResponseRefresher<MyExpiringResponse>() {
+
+            @Override
+            public MyExpiringResponse refresh(MyExpiringResponse previous) {
+                return new MyExpiringResponse();
+            }
+            
+        };
+        this.refreshableResponseProvider = new RefreshableResponseProvider<MyExpiringResponse>(
+         refreshIntervalMillis,
+         initialToken,
+         refreshTokenFunction);
+
+        for (int i = 0; i < 10; i++) {
+            long actualNextRefreshInterval = refreshableResponseProvider.nextRefreshInterval();
+            assertTrue("actualNextRefreshInterval "+actualNextRefreshInterval
+                    +" didn't match refreshIntervalMillis "+refreshIntervalMillis,
+                    refreshIntervalMillis == actualNextRefreshInterval);
+        }
+    }
+    
+    @Test
+    public void test_refreshToken_fails() throws InterruptedException {
+        refreshIntervalMillis = 100L;
+        initialToken = new MyExpiringResponse();
+        refreshTokenFunction = new ResponseRefresher<MyExpiringResponse>() {
+
+            @Override
+            public MyExpiringResponse refresh(MyExpiringResponse previous) {
+                throw new RuntimeException("simulate unable to refresh");
+            }
+            
+        };
+        this.refreshableResponseProvider = new RefreshableResponseProvider<MyExpiringResponse>(
+         refreshIntervalMillis,
+         initialToken,
+         refreshTokenFunction);
+
+        
+        for (int i = 0; i < 10; i++) {
+            Thread.sleep(100L);
+            MyExpiringResponse response = refreshableResponseProvider.getUnexpiredResponse();
+            assertTrue("response was null", null != response);
+            long expiresIn = response.getExpiresIn();
+            long expectedExpiresIn = 600L;
+            assertTrue("expected expires in "+expectedExpiresIn+" != actual expiresIn "+expiresIn, 
+                    expectedExpiresIn == expiresIn);
+        }
+    }
+
+    
     
     @Test
     public void test_shutdown_multiple() {
