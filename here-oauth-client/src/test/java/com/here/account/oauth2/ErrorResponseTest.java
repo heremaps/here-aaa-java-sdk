54783c2b43a5bfb47fa987e209d93c7cdb30f14c
Kenneth McCracken <ken.mccracken@here.com>
2019-01-08 21:29:31 -0500

    ErrorResponse: add additional test for 6-arg constructor.
    Signed-off-by: Kenneth McCracken <ken.mccracken@here.com>

--- here-oauth-client/src/test/java/com/here/account/oauth2/ErrorResponseTest.java
+++ here-oauth-client/src/test/java/com/here/account/oauth2/ErrorResponseTest.java
@@ -19,9 +19,75 @@ public class ErrorResponseTest {
     private String action = "action";
     private String correlationId = "correlationId";
 
+    ErrorResponse errorResponse;
+
+    @Test
+    public void test_6arg_constructor() {
+        errorResponse = new ErrorResponse(error,
+                errorDescription,
+                errorId,
+                httpStatus,
+                errorCode,
+                message);
+
+        verifyFirst6Properties();
+
+        String actualTitle = errorResponse.getTitle();
+        assertTrue("expected title " + null + ", actual " + actualTitle,
+                null == actualTitle);
+
+        Integer actualStatus = errorResponse.getStatus();
+        assertTrue("expected status " + null + ", actual " + actualStatus,
+                null == (actualStatus));
+
+        String actualCode = errorResponse.getCode();
+        assertTrue("expected code " + null + ", actual " + actualCode,
+                null == (actualCode));
+
+        String actualCause = errorResponse.getCause();
+        assertTrue("expected cause " + null + ", actual " + actualCause,
+                null == (actualCause));
+
+        String actualAction = errorResponse.getAction();
+        assertTrue("expected action " + null + ", actual " + actualAction,
+                null == (actualAction));
+
+        String actualCorrelationId = errorResponse.getCorrelationId();
+        assertTrue("expected correlationId " + correlationId + ", actual " + actualCorrelationId,
+                null == (actualCorrelationId));
+
+    }
+
+    private void verifyFirst6Properties() {
+        String actualError = errorResponse.getError();
+        assertTrue("expected error " + error + ", actual " + actualError,
+                error.equals(actualError));
+
+        String actualErrorDescription = errorResponse.getErrorDescription();
+        assertTrue("expected errorDescription " + errorDescription + ", actual " + actualErrorDescription,
+                errorDescription.equals(actualErrorDescription));
+
+        String actualErrorId = errorResponse.getErrorId();
+        assertTrue("expected errorId " + errorId + ", actual " + actualErrorId,
+                errorId.equals(actualErrorId));
+
+        Integer actualHttpStatus = errorResponse.getHttpStatus();
+        assertTrue("expected httpStatus " + httpStatus + ", actual " + actualHttpStatus,
+                httpStatus.equals(actualHttpStatus));
+
+        Integer actualErrorCode = errorResponse.getErrorCode();
+        assertTrue("expected errorCode " + errorCode + ", actual " + actualErrorCode,
+                errorCode.equals(actualErrorCode));
+
+        String actualMessage = errorResponse.getMessage();
+        assertTrue("expected message " + message + ", actual " + actualMessage,
+                message.equals(actualMessage));
+
+    }
+
     @Test
     public void test_correlationId() {
-        ErrorResponse errorResponse = new ErrorResponse(error,
+        errorResponse = new ErrorResponse(error,
                 errorDescription,
                 errorId,
                 httpStatus,
@@ -33,6 +99,9 @@ public class ErrorResponseTest {
                 cause,
                 action,
                 correlationId);
+
+        verifyFirst6Properties();
+
         String actualTitle = errorResponse.getTitle();
         assertTrue("expected title " + title + ", actual " + actualTitle,
                 title.equals(actualTitle));
